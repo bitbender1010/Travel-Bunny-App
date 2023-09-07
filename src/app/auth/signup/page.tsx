f49fe1c107b6d3186a31c3ecb337a600b3df98a4
bitbender1010 <bitbender1010@gmail.com>
2023-09-07 15:21:22 +0100

    upate

--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -5,22 +5,22 @@ import style from '../../create.module.css';
 
 const SignUp = () => (
     <main>
-        <div className='pls'>Please enter your information</div>
+        <div className='pls'>Please enter your information :)</div>
 
         {/* Form */}
         <form name="contact-form" method="POST" data-netlify="true">
             <input type="hidden" name="form-name" value="contact-form" />
 
+            <label>Email Address</label>
+            <input type='text' name='email-address'/>
+            
             <label>First Name</label>
             <input type='text' name='first-name'/>
 
             <label>Last Name</label>
             <input type='text' name='last-name'/>
 
-            <label>Email Address</label>
-            <input type='text' name='email-address'/>
-
-            <button type='submit'> Get Early Access </button>
+            <button type='submit' className='gea'> Get Early Access </button>
         </form>
 
     </main>
